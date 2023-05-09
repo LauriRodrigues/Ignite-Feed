6e306044e4ac445cb8d34c44bab06d338dc577c5
LauriRodrigues <laurianarodrigueslima@gmail.com>
2023-05-09 16:58:52 -0300

    post component & header att

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './global.css'
 import styles from './App.module.css'
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
+import { Post } from './components/Post'
 
 export function App() {
 
@@ -11,7 +12,10 @@ export function App() {
 
       <div className={styles.wrapper}>
         <Sidebar/>
-        <main>posts</main>
+        <main>
+          <Post/>
+          <Post/>
+        </main>
       </div>
     </>
   )
