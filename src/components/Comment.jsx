d68475ac7f84568f634c4b2e778a9519bd0ccbcb
LauriRodrigues <laurianarodrigueslima@gmail.com>
2023-05-10 16:47:17 -0300

    comment att

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -18,13 +18,13 @@ export function Comment({content, onDeleteComment}) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/diego3g.png"/>
+      <Avatar hasBorder={false} src="https://github.com/LauriRodrigues.png"/>
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <strong>Diego Fernandes</strong>
+              <strong>Lauri Rodrigues</strong>
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
             </div>
             <button 
