dbf5aac8b986549efb191ca2c23d325d2b83b011
LauriRodrigues <laurianarodrigueslima@gmail.com>
2023-05-10 15:22:06 -0300

    delete comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -26,6 +26,14 @@ export function Post({author, content, publishedAt}) {
     setNewCommentText(event.target.value)
   }
 
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment !== commentToDelete
+    })
+
+    setComments(commentsWithoutDeletedOne)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -70,7 +78,13 @@ export function Post({author, content, publishedAt}) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment} content={comment}/>
+          return (
+            <Comment 
+              key={comment} 
+              content={comment} 
+              onDeleteComment={deleteComment}
+            />
+          )
         })}
       </div>
 
